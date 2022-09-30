945ef06152f8ccda4538c07daf81b1e64c7ecdd6
aprilte <apriltexs@gmail.com>
2022-10-01 06:47:02 +0900

    1001::1

---
+++ Topics/Characters/Digits or not/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // write your code here
+    repeat(4) {
+        if (readln() in "0".."9") {
+            println("true")
+        } else {
+            println("false")
+        }
+    }
+}
